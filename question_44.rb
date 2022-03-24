3a8907bd159f0b537cc8c50741b5a5d0b61ffdef
Joji <freedive.cebu30@gmail.com>
2022-03-24 21:54:18 +0800

    Update question_44.rb

--- question_44.rb
+++ question_44.rb
@@ -2,6 +2,5 @@ h_fruits = { "apple" => 100, "mango" => 200, "banana" => 400, "orange" => 300 }
 val = h_fruits.max_by { |key, value| value }
 p (val)
 
-
 val = h_fruits.max { |(key1, value1), (key2, value2)| value1 <=> value2 }
 p(val)
