27560f3026f0dbea59d473eaa47eb2d3b492f076
Joji <freedive.cebu30@gmail.com>
2022-03-24 21:37:38 +0800

    Update question_32.rb

--- question_32.rb
+++ question_32.rb
@@ -3,12 +3,10 @@ val = []
   val << rand(1..10)
 end
 
-
 # 配列の要素数は20です
 # the number of element in Array is 20
 p(val.size)
 p(val.uniq!.sort!)
-
 
 # 配列に入れる前に、既に値が入っているか確認しています
 # We check the value in Array before adding the value
@@ -17,10 +15,8 @@ val = []
   rand_val = rand(1..10)
   val << rand_val unless val.include?(rand_val)
 end
-
 p(val.size)
 p(val.sort!)
-
 
 # Setという概念を使えば、重複しない集合を作ることができます
 # We can create a set without repetition if we use "Set"
