024640a564caa1dc005449ee0e62574ab73b0143
Joji <freedive.cebu30@gmail.com>
2022-03-24 22:49:33 +0800

    Update question_47.rb

--- question_47.rb
+++ question_47.rb
@@ -6,7 +6,6 @@ def check_3_5_multiple_1(number)
   end
 end
 
-
 def check_3_5_multiple_2(number)
   if number % 3 != 0 and number % 5 != 0
     false
@@ -15,17 +14,14 @@ def check_3_5_multiple_2(number)
   end
 end
 
-
 puts(check_3_5_multiple_1(9))
 puts(check_3_5_multiple_1(10))
 puts(check_3_5_multiple_1(14))
 puts(check_3_5_multiple_1(15))
-puts(check_3_5_multiple_1(16))
 puts(check_3_5_multiple_1(30))
 
 puts(check_3_5_multiple_2(9))
 puts(check_3_5_multiple_2(10))
 puts(check_3_5_multiple_2(14))
 puts(check_3_5_multiple_2(15))
-puts(check_3_5_multiple_2(16))
 puts(check_3_5_multiple_2(30))
