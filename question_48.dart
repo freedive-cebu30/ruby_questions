a602314c302e66e5b585c7a22d6e519b6620f6c4
Joji <freedive.cebu30@gmail.com>
2022-04-06 17:42:22 +0800

    Delete question_48.dart

--- question_48.dart
+++
@@ -1,9 +0,0 @@
-main() {
-  List<String> fruits = ["apple", "apple", "orange", "mango", "mango", "mango", "mango"];
-  Map mapFruits = {};
-  for (var fruit in fruits) {
-    mapFruits[fruit] = mapFruits[fruit] ?? 0;
-    mapFruits[fruit] += 1;
-  }
-  print(mapFruits);
-}
